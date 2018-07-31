7731b31ac8da361f92d479a13cd453311d9cad37
Ademola Oyewale <saopayne@gmail.com>
2018-07-31 09:03:21 +0100

    Add solution for product of array except self

---
+++ Leetcode/ProdArrExceptSelf.java
@@ -0,0 +1,24 @@
+
+public class ProdArrExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= right;
+            right *= nums[i];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,3,4};
+        ProdArrExceptSelf cv = new ProdArrExceptSelf();
+        System.out.println(cv.productExceptSelf(nums));
+    }
+}
